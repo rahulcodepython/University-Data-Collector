15cfb62c0586a0615d6eb86c08f4b754e1ac655a
Rahul Das <77882624+rahulcodepython@users.noreply.github.com>
2024-08-06 14:46:15 +0530

    Update: Uncomment important code.

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -3,20 +3,19 @@ import Record from "@/server/schema/record";
 
 export async function POST(req: Request) {
     try {
-        // await connect();
+        await connect();
 
-        // const body = await req.json();
+        const body = await req.json();
 
-        // const record = await Record.findOne({ student_code: body.student_code });
+        const record = await Record.findOne({ student_code: body.student_code });
 
-        // if (record) {
-        //     await Record.replaceOne({ student_code: body.student_code }, body);
-        // } else {
-        //     await Record.create(body);
-        // }
+        if (record) {
+            await Record.replaceOne({ student_code: body.student_code }, body);
+        } else {
+            await Record.create(body);
+        }
 
-        // return Response.json({ message: "Record is submitted successfully." });
-        return Response.json({ error: "Internal Server Error" }, { status: 500 });
+        return Response.json({ message: "Record is submitted successfully." });
 
     } catch (error) {
         return Response.json({ error: "Internal Server Error" }, { status: 500 });
